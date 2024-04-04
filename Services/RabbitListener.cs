d890ff9d3fcbadbc6a38f6866dfa4151240e0a75
Mungert69 <contact@mahadeva.co.uk>
2024-04-05 00:16:40 +0200

    first commit

---
+++ Services/RabbitListener.cs
@@ -0,0 +1,263 @@
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using NetworkMonitor.Objects.ServiceMessage;
+using NetworkMonitor.Objects;
+using NetworkMonitor.Data.Services;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using NetworkMonitor.Utils;
+using Microsoft.Extensions.Logging;
+using NetworkMonitor.Objects.Factory;
+using NetworkMonitor.Utils.Helpers;
+using NetworkMonitor.Objects.Repository;
+using System.Net;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using OneOf.Types;
+namespace NetworkMonitor.LLM.Services;
+
+public interface IRabbitListener
+{
+
+    Task<ResultObj> StartSession(LLMServiceObj? llmServiceObj);
+    Task<ResultObj> UserInput(LLMServiceObj? llmServiceObj);
+
+
+}
+
+public class RabbitListener : RabbitListenerBase, IRabbitListener
+{
+    protected ILLMService _llmService;
+
+    public RabbitListener(ILLMService llmService, ILogger<RabbitListenerBase> logger, ISystemParamsHelper systemParamsHelper) : base(logger, DeriveSystemUrl(systemParamsHelper))
+    {
+
+        _llmService = llmService;
+        Setup();
+    }
+
+    private static SystemUrl DeriveSystemUrl(ISystemParamsHelper systemParamsHelper)
+    {
+        return systemParamsHelper.GetSystemParams().ThisSystemUrl;
+    }
+    protected override void InitRabbitMQObjs()
+    {
+
+
+      
+      
+        _rabbitMQObjs.Add(new RabbitMQObj()
+        {
+            ExchangeName = "llmStartSession",
+            FuncName = "llmStartSession",
+            MessageTimeout = 60000
+        });
+        _rabbitMQObjs.Add(new RabbitMQObj()
+        {
+            ExchangeName = "llmUserInput",
+            FuncName = "llmUserInput",
+            MessageTimeout = 60000
+        });
+        _rabbitMQObjs.Add(new RabbitMQObj()
+        {
+            ExchangeName = "llmRemoveSession",
+            FuncName = "llmRemoveSession",
+            MessageTimeout = 60000
+        });
+
+
+
+    }
+    protected override ResultObj DeclareConsumers()
+    {
+        var result = new ResultObj();
+        result.Success = true;
+        try
+        {
+            _rabbitMQObjs.ForEach(rabbitMQObj =>
+        {
+            if (rabbitMQObj.ConnectChannel == null)
+            {
+                result.Message += $" Error : RabbitListener Connect Channel not open for Exchange {rabbitMQObj.ExchangeName}";
+                result.Success = false;
+                _logger.LogCritical(result.Message);
+                return;
+            }
+            rabbitMQObj.Consumer = new EventingBasicConsumer(rabbitMQObj.ConnectChannel);
+
+            if (rabbitMQObj.Consumer == null)
+            {
+                result.Message += $" Error : RabbitListener can't create Consumer for queue  {rabbitMQObj.QueueName}";
+                result.Success = false;
+                _logger.LogCritical(result.Message);
+                return;
+            }
+            switch (rabbitMQObj.FuncName)
+            {
+                case "llmStartSession":
+                    rabbitMQObj.ConnectChannel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+                    rabbitMQObj.Consumer.Received += async (model, ea) =>
+                {
+                    try
+                    {
+                        result = await StartSession(ConvertToObject<LLMServiceObj>(model, ea));
+                        rabbitMQObj.ConnectChannel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(" Error : RabbitListener.DeclareConsumers.llmStartSession " + ex.Message);
+                    }
+                };
+                    break;
+                case "llmRemoveSession":
+                    rabbitMQObj.ConnectChannel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+                    rabbitMQObj.Consumer.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        result = RemoveSession(ConvertToObject<LLMServiceObj>(model, ea));
+                        rabbitMQObj.ConnectChannel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(" Error : RabbitListener.DeclareConsumers.llmRemoveSession " + ex.Message);
+                    }
+                };
+                    break;
+                case "llmUserInput":
+                    rabbitMQObj.ConnectChannel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+                    rabbitMQObj.Consumer.Received += async (model, ea) =>
+                {
+                    try
+                    {
+                        result = await UserInput(ConvertToObject<LLMServiceObj>(model, ea));
+                        rabbitMQObj.ConnectChannel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(" Error : RabbitListener.DeclareConsumers.llmUserInput " + ex.Message);
+                    }
+                };
+                    break;
+
+            }
+        });
+            if (result.Success) result.Message += " Success : Declared all consumers ";
+        }
+        catch (Exception e)
+        {
+            string message = " Error : failed to declare consumers. Error was : " + e.ToString() + " . ";
+            result.Message += message;
+            _logger.LogError(result.Message);
+            result.Success = false;
+        }
+        return result;
+    }
+    public async Task<ResultObj> StartSession(LLMServiceObj? llmServiceObj)
+    {
+        var result = new ResultObj();
+        result.Success = false;
+        result.Message = "MessageAPI : StartSession : ";
+        if (llmServiceObj == null)
+        {
+            result.Message += " Error : llmServiceObj is null.";
+            _logger.LogError(result.Message);
+            result.Success = false;
+            return result;
+        }
+
+        try
+        {
+            llmServiceObj = await _llmService.StartProcess(llmServiceObj);
+            result.Message = llmServiceObj.ResultMessage;
+            result.Success = llmServiceObj.ResultSuccess;
+
+
+        }
+        catch (Exception e)
+        {
+            result.Message = e.Message;
+            result.Success = false;
+        }
+
+        if (result.Success) _logger.LogInformation(result.Message);
+        else _logger.LogError(result.Message);
+        return result;
+    }
+
+    public ResultObj RemoveSession(LLMServiceObj? llmServiceObj)
+    {
+        var result = new ResultObj();
+        result.Success = false;
+        result.Message = "MessageAPI : RemoveSession : ";
+        if (llmServiceObj == null)
+        {
+            return new ResultObj() { Message = " Error : llmServiceObj is null." };
+        }
+
+        try
+        {
+            llmServiceObj = _llmService.RemoveProcess(llmServiceObj);
+            result.Message = llmServiceObj.ResultMessage;
+            result.Success = llmServiceObj.ResultSuccess;
+
+
+        }
+        catch (Exception e)
+        {
+            result.Message = e.Message;
+            result.Success = false;
+        }
+
+        if (result.Success) _logger.LogInformation(result.Message);
+        else _logger.LogError(result.Message);
+        return result;
+    }
+
+
+    public async Task<ResultObj> UserInput(LLMServiceObj? serviceObj)
+    {
+        var result = new ResultObj();
+        result.Success = false;
+        result.Message = "MessageAPI : UserInput : ";
+        _logger.LogInformation($" Start User Input {serviceObj!.UserInput}");
+        if (serviceObj == null )
+        {
+            result.Message += " Error : serviceObj is null.";
+            _logger.LogError(result.Message);
+            result.Success = false;
+            return result;
+        }
+          if (serviceObj.UserInput == "" )
+        {
+            result.Message += " Error : serviceObj.UserInput is empty.";
+            _logger.LogError(result.Message);
+            result.Success = false;
+            return result;
+        }
+
+        try
+        {
+            if (serviceObj.IsFunctionCallResponse)
+            {
+                serviceObj.UserInput = "FUNCTION RESPONSE: " + serviceObj.UserInput;
+            }
+            var resultService = await _llmService.SendInputAndGetResponse(serviceObj);
+            result.Message += resultService.Message;
+            result.Success = resultService.Success;
+        }
+        catch (Exception e)
+        {
+            result.Message = e.Message;
+            result.Success = false;
+
+        }
+        if (result.Success) _logger.LogInformation(result.Message);
+        else _logger.LogError(result.Message);
+        return result;
+    }
+
+}
---
+++ Startup.cs
@@ -0,0 +1,92 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NetworkMonitor.LLM.Services;
+using NetworkMonitor.Data;
+using NetworkMonitor.Objects;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using NetworkMonitor.Utils;
+using NetworkMonitor.Objects.Factory;
+using NetworkMonitor.Objects.Repository;
+using HostInitActions;
+using Microsoft.Extensions.Logging;
+using NetworkMonitor.Utils.Helpers;
+using NetworkMonitor.Objects.ServiceMessage;
+namespace NetworkMonitor.LLM
+{
+    public class Startup
+    {
+        private readonly CancellationTokenSource _cancellationTokenSource;
+        #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+        public Startup(IConfiguration configuration)
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            Configuration = configuration;
+        }
+        #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+        public IConfiguration Configuration { get; }
+        private IServiceCollection _services;
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            _services = services;
+            services.AddLogging(builder =>
+                          {
+                              builder.AddSimpleConsole(options =>
+                        {
+                            options.TimestampFormat = "yyyy-MM-dd HH:mm:ss ";
+                            options.IncludeScopes = true;
+                        });
+
+                          });
+            string connectionString = Configuration.GetConnectionString("DefaultConnection") ?? "";
+            services.AddDbContext<MonitorContext>(options =>
+                options.UseMySql(connectionString,
+                ServerVersion.AutoDetect(connectionString),
+                mySqlOptions =>
+                     {
+                         mySqlOptions.EnableRetryOnFailure(
+                         maxRetryCount: 5,
+                         maxRetryDelay: TimeSpan.FromSeconds(10),
+                         errorNumbersToAdd: null);
+                         mySqlOptions.CommandTimeout(600);  // Set to 600 seconds, for example
+                     }
+            ));
+            services.AddSingleton<IRabbitListener, RabbitListener>();
+            services.AddSingleton<IRabbitRepo, RabbitRepo>();
+            services.AddSingleton<IFileRepo, FileRepo>();
+            services.AddSingleton<ISystemParamsHelper, SystemParamsHelper>();
+             services.AddSingleton<ILLMResponseProcessor, LLMResponseProcessor>();
+             services.AddSingleton<ILLMService, LLMService>();
+             services.AddSingleton<ILLMProcessRunner, LLMProcessRunner>();
+
+            services.AddSingleton(_cancellationTokenSource);
+            services.Configure<HostOptions>(s => s.ShutdownTimeout = TimeSpan.FromMinutes(5));
+            services.AddAsyncServiceInitialization()
+                
+                 .AddInitAction<IRabbitListener>((rabbitListener) =>
+                    {
+                        return Task.CompletedTask;
+                    });
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime appLifetime)
+        {
+
+            appLifetime.ApplicationStopping.Register(() =>
+            {
+                _cancellationTokenSource.Cancel();
+            });
+
+        }
+    }
+}
