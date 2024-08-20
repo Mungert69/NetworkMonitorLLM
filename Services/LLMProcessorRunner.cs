99a10e095cd0972f94801a5dd4556dbc4506564f
Mungert69 <contact@mahadeva.co.uk>
2024-08-20 19:27:54 +0200

    break on condition changed to<|content|>You are a

--- Services/LLMProcessorRunner.cs
+++ Services/LLMProcessorRunner.cs
@@ -205,7 +205,7 @@ public class LLMProcessRunner : ILLMRunner
         while (!cancellationTokenSource.IsCancellationRequested)
         {
             line = await process.StandardOutput.ReadLineAsync();
-            if (line.StartsWith("<|content|>You are a"))
+            if (line.Contains("#READY#"))
             {
                 isReady = true;
                 break;
