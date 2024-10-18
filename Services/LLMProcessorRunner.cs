334a6bc72bff495ea7aceec0a9ba8694f8e0e039
Mungert69 <contact@mahadeva.co.uk>
2024-10-17 19:34:27 -0500

    allow 2.4 to detect end of system prompt

--- Services/LLMProcessorRunner.cs
+++ Services/LLMProcessorRunner.cs
@@ -254,7 +254,7 @@ public class LLMProcessRunner : ILLMRunner
         while (!cancellationTokenSource.IsCancellationRequested)
         {
             line = await process.StandardOutput.ReadLineAsync();
-            if (line.StartsWith("<|eot_id|>"))
+            if (line.StartsWith("<|eot_id|>") || line.Contains("<|LLM_STARTED|>"))
             {
                 isReady = true;
                 break;
