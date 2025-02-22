10eea6fb544c89db85f61a570073b9775d32a423
Mungert69 <contact@mahadevac.co.uk>
2025-02-22 21:52:34 +0100

    removed logging

--- Services/Api/ChatResponseBuilder.cs
+++ Services/Api/ChatResponseBuilder.cs
@@ -151,8 +151,8 @@ public class ChatResponseBuilder
             Id = responseObject.Id,
             Model = responseObject.Model
         };
-        string payloadJson = JsonConvert.SerializeObject(chatResponse, Formatting.Indented);
-        _logger.LogInformation($"{payloadJson}");
+        //string payloadJson = JsonConvert.SerializeObject(chatResponse, Formatting.Indented);
+        //_logger.LogInformation($"{payloadJson}");
         return chatResponse;
     }
 
