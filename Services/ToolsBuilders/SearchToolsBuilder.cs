a938882acb305ff42ffa0cfdfb7c357e47b15195
Mungert69 <contact@mahadevac.co.uk>
2025-02-25 01:23:30 +0100

    updated func defs

--- Services/ToolsBuilders/SearchToolsBuilder.cs
+++ Services/ToolsBuilders/SearchToolsBuilder.cs
@@ -21,11 +21,12 @@ namespace NetworkMonitor.LLM.Services
         {
 
             fn_run_search_web = new FunctionDefinitionBuilder("run_search_web",
-            "Search function to gather information from web sources. Use this function to perform a Google search and retrieve a list of websites related to the search term. After retrieving the URLs, you must call the 'run_crawl_page' function to visit and gather details from each site. The search results are intended to guide the selection of relevant links for deeper exploration.")
+            "Search function to gather information from web sources. Use this function to perform a Google search. It will either return the full page content and links from the top pages in the search or just the urls of those pages")
             .AddParameter("search_term", PropertyDefinition.DefineString("The search term to be used for the Google search."))
+            .AddParameter("return_only_urls", PropertyDefinition.DefineString("Should the function call return only a list of urls that match the search term. The default is false ie return the full content of all pages found."))
             .AddParameter("agent_location", PropertyDefinition.DefineString("The agent location that will execute the command, optional. Specify which agent will perform the operation if relevant."))
-            .AddParameter("number_lines", PropertyDefinition.DefineInteger("Number of lines to return from the command output. Limit this to manage the amount of search results returned. Larger values may retrieve more links, but use higher limits cautiously."))
-            .AddParameter("page", PropertyDefinition.DefineInteger("The page of search results to return, allowing pagination through multiple search results pages."))
+            .AddParameter("number_lines", PropertyDefinition.DefineInteger("Number of lines of page content to return. Limit this to manage the amount data return from the search."))
+            .AddParameter("page", PropertyDefinition.DefineInteger("If data is truncated use pages to allow pagination through the data."))
             .Validate()
             .Build();
 
@@ -34,9 +35,9 @@ namespace NetworkMonitor.LLM.Services
                    "Website crawler to extract information from a webpage. Use this function to read the text and hyperlinks on a given webpage. When URLs are returned from 'run_search_web', call this function on relevant URLs to gather content. If necessary, you can follow additional links on the page to perform further research.")
                    .AddParameter("url", PropertyDefinition.DefineString("The URL of the page to crawl."))
                    .AddParameter("agent_location", PropertyDefinition.DefineString("The agent location that will execute the command, optional. Specify which agent will perform the operation if relevant."))
-                   .AddParameter("number_lines", PropertyDefinition.DefineInteger("Number of lines to return from the command output. Limit this to manage the amount of content returned."))
-                   .AddParameter("page", PropertyDefinition.DefineInteger("The page of content to return, allowing pagination through large pages of data."))
-                   .Validate()
+                    .AddParameter("number_lines", PropertyDefinition.DefineInteger("Number of lines of page content to return. Limit this to manage the amount data return from the page."))
+                    .AddParameter("page", PropertyDefinition.DefineInteger("If data is truncated use pages to allow pagination through the data."))
+                    .Validate()
                    .Build();
 
 
@@ -81,7 +82,7 @@ When responding to queries:
 5. Cite sources and provide a structured summary of findings.
 6. Be prepared for follow-up questions, using additional searches or page crawls as needed.
 
-Remember to use the functions responsibly and ethically, respecting user privacy and intellectual property rights. Always strive to provide accurate, up-to-date, and well-researched responses to user queries." 
+Remember to use the functions responsibly and ethically, respecting user privacy and intellectual property rights. Always strive to provide accurate, up-to-date, and well-researched responses to user queries."
 + $" The current time is{currentTime}.";
 
             var chatMessage = ChatMessage.FromSystem(content);
