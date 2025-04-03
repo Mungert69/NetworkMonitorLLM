fce269f79114dcd8b7239b64a8bed1bb09c5459b
Mungert69 <contact@gmail.com>
2025-04-03 06:26:01 +0200

    .

--- Services/ToolsBuilders/MonitorToolsBuilder.cs
+++ Services/ToolsBuilders/MonitorToolsBuilder.cs
@@ -49,11 +49,11 @@ public class MonitorToolsBuilder : ToolsBuilderBase
         fn_call_quantum_expert = BuildCallQuantumFunction();
 
 
-         // Static tools list assignment
+        // Static tools list assignment
         _tools = new List<ToolDefinition>()
         {
-            new ToolDefinition() { Function = fn_call_security_expert, Type = "function" },
-             new ToolDefinition() { Function = fn_are_functions_running, Type = "function" },
+
+            new ToolDefinition() { Function = fn_are_functions_running, Type = "function" },
             new ToolDefinition() { Function = fn_cancel_functions, Type = "function" },
             new ToolDefinition() { Function = fn_add_host, Type = "function" },
             new ToolDefinition() { Function = fn_edit_host, Type = "function" },
@@ -61,6 +61,8 @@ public class MonitorToolsBuilder : ToolsBuilderBase
             new ToolDefinition() { Function = fn_get_host_list, Type = "function" },
             new ToolDefinition() { Function = fn_get_user_info, Type = "function" },
             new ToolDefinition() { Function = fn_get_agents, Type = "function" },
+            new ToolDefinition() { Function = fn_call_security_expert, Type = "function" },
+            new ToolDefinition() { Function = fn_call_penetration_expert, Type = "function" },
             new ToolDefinition() { Function = fn_call_search_expert, Type = "function" },
             new ToolDefinition() { Function = fn_call_cmd_processor_expert, Type = "function" },
             new ToolDefinition() { Function = fn_call_quantum_expert, Type = "function" }
@@ -244,12 +246,12 @@ public class MonitorToolsBuilder : ToolsBuilderBase
         string content = $"You are a network monitoring and security assistant. Use the tools where necessary to assist the user. Your name is {llmType}, and you are faster than HugLLM.";
 
         content += "When calling functions ONLY include parameters that are strictly necessary. DO NOT include parameters set to null or empty. ONLY include parameters you set to to a value. If a function call fails or returns incomplete data, provide feedback to the user before attempting the call again or trying a different tool.";
-       content += " Always adhere to security and privacy best practices when handling sensitive network or user data. Do not display or log confidential information unnecessarily.";
+        content += " Always adhere to security and privacy best practices when handling sensitive network or user data. Do not display or log confidential information unnecessarily.";
         content += "Before allowing the user to run penetration tests, network scans or busybox commands, you must get explicit confirmation from them that they understand and agree that these tools can only be used on servers they own or are authorized to test. Do not allow these functions to be called unless the user confirms their compliance. DO NOT keep asking the user for compliance once they have accepted it.";
         content += "The Experts are to be treated as another person in the conversation. Work with the expert to fulfil the users requests. Its ok to have a conversation with the expert without consulting the user as long as it fulfils the users request.";
         content += "When calling the experts take note that the expert does not have access to your conversation with the user so you must give it all the information it needs to fulfil the user request. YOU MUST give it information from the current conversation as it does not have access to this. An example would be something the user has said or requested needs to be passed to the expert as it does not know what the user has said.";
         content += "When choosing which tools to call be aware of the difference between ongoing monitoring tools like adding add_host, edit_host, get_host_data and get_host_list and tools that are run immediately like the call experts, run busybox, cancel and are functions running. The monitoring tools run continuously and provide realtime monitoring. The rest of the functions are, one hit, call and get result.  An example would be use the monitoring functions if the user wanted to monitor a website or keep checking if their server was quantum safe ie add a host with end point type set to quantum. However If they wanted to perform a one of quantum test then call the quantum expert. There are also tools to get the current user info and agents that are used for both monitoring and one hit calls";
-        
+
         var chatMessage = new ChatMessage()
         {
             Role = "system",
