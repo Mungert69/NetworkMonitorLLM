d8f59314d36a7199fd0aa6f2fe5504c1aaf6fc56
Mungert69 <contact@mahadeva.co.uk>
2025-06-22 03:05:08 +0200

    added new secutiry agents

--- Services/ToolsBuilders/Agents/SecutiryAgentNodeToolsBuilder.cs
+++ Services/ToolsBuilders/Agents/SecutiryAgentNodeToolsBuilder.cs
@@ -1,6 +1,11 @@
 using NetworkMonitor.Objects.ServiceMessage;
 using NetworkMonitor.Utils;
+using Betalgo.Ranul.OpenAI;
+using Betalgo.Ranul.OpenAI.Builders;
+using Betalgo.Ranul.OpenAI.Managers;
 using Betalgo.Ranul.OpenAI.ObjectModels;
+using Betalgo.Ranul.OpenAI.ObjectModels.RequestModels;
+using Betalgo.Ranul.OpenAI.ObjectModels.SharedModels;
 using System;
 using System.Collections.Generic;
 
