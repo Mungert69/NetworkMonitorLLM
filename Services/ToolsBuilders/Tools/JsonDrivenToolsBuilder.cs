201733bc7a95b70f4bc9ff3468e2554a4b29741d
Mungert69 <contact@mahadeva.co.uk>
2025-06-26 00:57:20 +0200

    fixed parameter name

--- Services/ToolsBuilders/Tools/JsonDrivenToolsBuilder.cs
+++ Services/ToolsBuilders/Tools/JsonDrivenToolsBuilder.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System;
 using System.Text.Json;
+using NetworkMonitor.Objects;
 namespace NetworkMonitor.LLM.Services;
 
 public class JsonDrivenToolsBuilder : ToolsBuilderBase
