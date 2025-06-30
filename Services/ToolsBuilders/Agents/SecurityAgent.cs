329c55672c12e26f2f5f1f030592ecfb4db84f5c
Mungert69 <contact@mahadeva.co.uk>
2025-06-30 17:53:50 +0200

    more system prompt updates to support reports being passed on

--- Services/ToolsBuilders/Agents/SecurityAgent.cs
+++ Services/ToolsBuilders/Agents/SecurityAgent.cs
@@ -21,9 +21,9 @@ public class SecurityAgent
     {
         return new FunctionDefinitionBuilder(
                 "call_security_basic_flow",
-                "Start a multi-step flow that performs a basic security assessment on the specified target. " +
-                "Only call this function if the user clearly indicates they want to initiate a security assessment flow." +
-                "The result from this function call will be professionally presented report. Pass the report back to the user without any modifications.")
+                "Security Basic Flow Expert. Use the Secutiry flow expert to start a multi-step flow that performs a basic security assessment on the specified target. " +
+                "Only call this function if the user clearly indicates they want to initiate a security flow ." +
+                "The result from this function call will be professionally presented report. Give the user the report without any modifications.")
                     .AddParameter(
                     "target",
                     PropertyDefinition.DefineString(
