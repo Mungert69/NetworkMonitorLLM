0134e28ee2a3ef4fb3c520cc4e91dc931d2e5c16
Mungert69 <contact@mahadeva.co.uk>
2026-02-18 02:35:07 +0000

    .

--- Services/Api/HuggingFaceApi.cs
+++ Services/Api/HuggingFaceApi.cs
@@ -71,7 +71,7 @@ public class HuggingFaceApi : ILLMApi
         _temperature = temperature;
         _modelVersion = mlParams.LlmHFModelVersion;
         _modelID = mlParams.LlmHFModelID;
-        _authToken = mlParams.LlmHFKey;
+        _authToken = mlParams.LlmHFKey;     
         _isXml = _mlParams.XmlFunctionParsing;
         _httpClient.DefaultRequestHeaders.Add("api-key", _authToken);
         _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _authToken);
@@ -153,7 +153,7 @@ public class HuggingFaceApi : ILLMApi
                 NullValueHandling = NullValueHandling.Ignore
             });
 
-            _logger.LogDebug($"{payloadJson}");
+            _logger.LogDebug($"PAYLOAD =====> {payloadJson}");
 
             if (!_isStream)
             {
