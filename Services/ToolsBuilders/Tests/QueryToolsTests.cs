98f48dcee4f4bdaf108744cc6081dbc1521be4cb
Mungert69 <contact@mahadeva.co.uk>
2026-03-14 22:25:36 +0000

    added actionable metadata query

--- Services/ToolsBuilders/Tests/QueryToolsTests.cs
+++ Services/ToolsBuilders/Tests/QueryToolsTests.cs
@@ -43,6 +43,14 @@ public class QueryToolsTests
         Assert.True(fn.Parameters.Properties.ContainsKey("anchor_doc_id"));
         Assert.True(fn.Parameters.Properties.ContainsKey("anchor_chunk_id"));
         Assert.True(fn.Parameters.Properties.ContainsKey("neighbor_window"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_doc_id"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_chunk_id"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_source_file"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_section_path"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_page_start"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_page_end"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_chunk_index_min"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_chunk_index_max"));
         Assert.False(fn.Parameters.Properties.ContainsKey("index_name"));
     }
 
@@ -59,6 +67,14 @@ public class QueryToolsTests
         Assert.True(fn.Parameters.Properties.ContainsKey("anchor_doc_id"));
         Assert.True(fn.Parameters.Properties.ContainsKey("anchor_chunk_id"));
         Assert.True(fn.Parameters.Properties.ContainsKey("neighbor_window"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_doc_id"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_chunk_id"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_source_file"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_section_path"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_page_start"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_page_end"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_chunk_index_min"));
+        Assert.True(fn.Parameters.Properties.ContainsKey("filter_chunk_index_max"));
         Assert.False(fn.Parameters.Properties.ContainsKey("index_name"));
     }
 
